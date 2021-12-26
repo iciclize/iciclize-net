5a7040a67cf55df591ff6ac5d510ac41f5b7dead
Yuto Ino <ino@iciclize.net>
2021-12-26 20:44:54 +0900

    Revert "fix build warnings"
    This reverts commit cce423b1d6fb3ae177d6a9b02745132f8c30bb12.

--- gatsby-node.js
+++ gatsby-node.js
@@ -44,8 +44,8 @@ const makeRequest = (graphql, request) =>
 
 // Implement the Gatsby API “createPages”. This is called once the
 // data layer is bootstrapped to let plugins create pages from data.
-exports.createPages = ({ actions, graphql }) => {
-  const { createPage } = actions
+exports.createPages = ({ boundActionCreators, graphql }) => {
+  const { createPage } = boundActionCreators
 
     console.log(process.env.NODE_ENV)
 
